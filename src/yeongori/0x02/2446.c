de44048c4008fba0583493c7af6098e37bc97690
root <root@DESKTOP-SLBATAP.localdomain>
2022-05-17 15:08:37 +0900

    0x02 done

--- src/yeongori/0x02/2446.c
+++ src/yeongori/0x02/2446.c
@@ -3,11 +3,12 @@
 
 void print(int cnt, int n)
 {
+
     int sp = n - cnt;
     for (int i = 0; i < 2 * n - 1; i++)
     {
-        if (sp == 0 && i > n)
-            printf(" ");
+        if (sp == 0 && i > cnt + n - 2)
+            ;
         else if (sp == 0)
             printf("*");
         else
@@ -23,10 +24,10 @@ void recursive(int cnt, int n)
 {
     if (cnt == 0)
         return;
-
     print(cnt, n);
     recursive(--cnt, n);
-    print(++cnt, n);
+    if (++cnt > 0)
+        print(++cnt, n);
 }
 
 int main(void)
