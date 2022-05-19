df268664994de2c919add3aa0927a388c3077535
jaeyounl <great.tit.jaejae@gmail.com>
2022-05-19 15:29:54 +0900

    09_boj_11328.cpp

--- src/jaeyoung/0x03/09_boj_11328.cpp
+++ src/jaeyoung/0x03/09_boj_11328.cpp
@@ -8,60 +8,33 @@ int main(void)
     
     int n;
     int i;
-    int j;
-    int k;
-    int l;
-    int cnt;
-    int arr2[255] = {};
-    int arr1[255] = {};
-    char **str = new char*[2001];
-
+    int num[26] = {};
+    int sum;
+    string s1, s2;
+    
+    i = 0;
     cin >> n;
-    i = 0;
+    while (n--)
+    {
+        cin >> s1 >> s2;
 
- //   str = (char **)malloc(sizeof(char *) * ((n * 2) + 1));
-//   if (!str)
-//       return (0);
-    while (i < (n * 2))
-    {
-        j = 0;
-        while (j < 1000)
-            cin >> str[i][j++];
-        i++;
-    }
-    str[n] = 0;
-    i = 0;
-    while (i < n)
-    {
-        j = i * 2;
-        k = 0;
-        while (str[j][k])
+        for(auto c : s1)
+            num[c - 'a']++;
+        for(auto c : s2)
+            num[c - 'a']--;
+        sum = 0;
+        for(auto c : num)
         {
-            if (str[j + 1] && arr2[(unsigned char)str[j + 1][k]] == 0)
-                arr2[(unsigned char)str[j + 1][k]] = 1;
-            if (str[j + 1] && arr2[(unsigned char)str[j + 1][k]])
-                arr2[(unsigned char)str[j + 1][k]]++;
-            if (str[j] && arr1[(unsigned char)str[j][k]] == 0)
-                arr1[(unsigned char)str[j][k]] = 1;
-            if (str[j] && arr1[(unsigned char)str[j][k]])
-                arr1[(unsigned char)str[j][k]]++;
-            k++;
+            if (c < 0)
+                c *= -1;
+            sum += c;
         }
-        l = 0;
-        cnt = 0;
-        while (l < 255)
-        {
-            if (arr1[l] == arr2[l])
-                cnt++;
-            l++;
-        }
-        if (cnt == 255)
-            cout << "Possible" << '\n';
-        else
+        if (sum)
             cout << "Impossible" << '\n';
-        arr1[255] = {};
-        arr2[255] = {};
-        i++;
+        else 
+            cout << "Possible" << '\n';
+        for(auto &c : num)
+            c = 0;
     }
 }
     
