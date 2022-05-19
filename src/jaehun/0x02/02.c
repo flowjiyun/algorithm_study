afb268260f37f40241ee79c23f18537600d51f03
jaehun yang <jaehun4321@gmail.com>
2022-05-19 23:58:19 +0900

    0

--- src/jaehun/0x02/02.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-//Hello World
-int main()
-{
-    printf("Hello World!");
-}
--- src/jaehun/0x02/03.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-//고양이
-int main()
-{
-    printf("\\    /\\\n");
-    printf(" )  ( ')\n");
-    printf("(  /  )\n");
-    printf(" \\(__)|\n");
-}
